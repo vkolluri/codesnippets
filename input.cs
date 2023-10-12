8063449abadbd19325ab58416e8342810a0c1531
vkolluri <31540929+vkolluri@users.noreply.github.com>
2023-10-12 15:01:29 -0400

    Update input.cs

--- input.cs
+++ input.cs
@@ -1,39 +1,29 @@
-Imports System
-Imports System.Data
-Imports Newtonsoft.Json.Linq
-
-Public Class JsonToDataTableConverter
-    Public Function ConvertJsonToDataTable(jsonString As String) As DataTable
+ Public Function DefineColumnsFromJson(jsonString As String) As DataTable
         Dim json As JObject = JObject.Parse(jsonString)
         Dim dt As New DataTable()
 
-        ' Define columns for DataTable
-        dt.Columns.Add("reportingEntityLocationId")
-        dt.Columns.Add("reportingEntityTransactionReference")
-        dt.Columns.Add("dateTimeOfTransaction")
-        dt.Columns.Add("methodCode")
-        dt.Columns.Add("amount")
-        dt.Columns.Add("currencyCode")
-        dt.Columns.Add("conductorTypeCode")
-        dt.Columns.Add("conductorRefId")
-        dt.Columns.Add("dispositionCode")
-
-        ' Iterate through transactions
-        For Each transaction As JObject In json("transactions")
-            Dim row As DataRow = dt.NewRow()
-            row("reportingEntityLocationId") = transaction("reportingEntityLocationId").ToString()
-            row("reportingEntityTransactionReference") = transaction("largeCashTransactionDetails")("reportingEntityTransactionReference").ToString()
-            row("dateTimeOfTransaction") = transaction("largeCashTransactionDetails")("dateTimeOfTransaction").ToString()
-            row("methodCode") = transaction("largeCashTransactionDetails")("methodCode").ToString()
-            row("amount") = transaction("startingActions")(0)("details")("amount").ToString()
-            row("currencyCode") = transaction("startingActions")(0)("details")("currencyCode").ToString()
-            row("conductorTypeCode") = transaction("startingActions")(0)("conductors")(0)("typeCode").ToString()
-            row("conductorRefId") = transaction("startingActions")(0)("conductors")(0)("refId").ToString()
-            row("dispositionCode") = transaction("completingActions")(0)("details")("dispositionCode").ToString()
-
-            dt.Rows.Add(row)
-        Next
+        AddColumnsFromJObject(json, dt, String.Empty)
 
         Return dt
     End Function
-End Class
+
+    Private Sub AddColumnsFromJObject(obj As JObject, dt As DataTable, parentName As String)
+        For Each property In obj.Properties()
+            Dim columnName As String
+            If String.IsNullOrEmpty(parentName) Then
+                columnName = property.Name
+            Else
+                columnName = parentName & "." & property.Name
+            End If
+
+            If TypeOf property.Value Is JObject Then
+                AddColumnsFromJObject(CType(property.Value, JObject), dt, columnName)
+            ElseIf TypeOf property.Value Is JArray AndAlso CType(property.Value, JArray).Count > 0 AndAlso TypeOf CType(property.Value, JArray)(0) Is JObject Then
+                AddColumnsFromJObject(CType(CType(property.Value, JArray)(0), JObject), dt, columnName)
+            Else
+                If Not dt.Columns.Contains(columnName) Then
+                    dt.Columns.Add(columnName)
+                End If
+            End If
+        Next
+    End Sub
