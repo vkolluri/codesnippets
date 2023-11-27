de6f56559be5602af7b5bad0be06c2a7c3f9b4a6
vkolluri <31540929+vkolluri@users.noreply.github.com>
2023-11-27 14:48:10 -0500

    Update input.cs

--- input.cs
+++ input.cs
@@ -9,48 +9,54 @@ class Program
     {
         string sourceConnectionString = "YourSourceConnectionString";
         string destinationConnectionString = "YourDestinationConnectionString";
-        string query = "SELECT * FROM SourceTable"; // Adjust your query here
-        int batchSize = 10000; // Batch size for reading data
+        string baseQuery = "SELECT * FROM SourceTable WHERE DateColumn >= @StartDate AND DateColumn < @EndDate";
+        
+        DateTime startDate = new DateTime(2020, 1, 1); // Start date of data transfer
+        DateTime endDate = new DateTime(2020, 12, 31); // End date of data transfer
+        TimeSpan batchInterval = TimeSpan.FromDays(1); // Interval for each batch
 
-        var bufferBlock = new BufferBlock<DataTable>(
-            new DataflowBlockOptions
-            {
-                BoundedCapacity = 2 // Adjust based on memory availability
-            });
-
-        var actionBlock = new ActionBlock<DataTable>(
-            data => BulkInsertData(data, destinationConnectionString),
-            new ExecutionDataflowBlockOptions
-            {
-                MaxDegreeOfParallelism = 1 // To maintain data integrity
-            });
-
-        bufferBlock.LinkTo(actionBlock, new DataflowLinkOptions { PropagateCompletion = true });
-
-        // Read and buffer data in batches
-        using (SqlConnection connection = new SqlConnection(sourceConnectionString))
+        for (DateTime batchStart = startDate; batchStart < endDate; batchStart += batchInterval)
         {
-            connection.Open();
-            using (SqlCommand command = new SqlCommand(query, connection))
-            {
-                SqlDataAdapter adapter = new SqlDataAdapter(command);
-                DataSet ds = new DataSet();
-                
-                int numberOfRowsRead = 0;
-                do
+            DateTime batchEnd = batchStart + batchInterval;
+            string query = baseQuery;
+
+            var bufferBlock = new BufferBlock<DataTable>(
+                new DataflowBlockOptions
                 {
-                    ds.Clear();
-                    numberOfRowsRead = adapter.Fill(ds, 0, batchSize, "Table");
-                    if (numberOfRowsRead > 0)
+                    BoundedCapacity = 2 // Adjust based on memory availability
+                });
+
+            var actionBlock = new ActionBlock<DataTable>(
+                data => BulkInsertData(data, destinationConnectionString),
+                new ExecutionDataflowBlockOptions
+                {
+                    MaxDegreeOfParallelism = 1 // To maintain data integrity
+                });
+
+            bufferBlock.LinkTo(actionBlock, new DataflowLinkOptions { PropagateCompletion = true });
+
+            // Read and buffer data for the current batch
+            using (SqlConnection connection = new SqlConnection(sourceConnectionString))
+            {
+                connection.Open();
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@StartDate", batchStart);
+                    command.Parameters.AddWithValue("@EndDate", batchEnd);
+
+                    SqlDataAdapter adapter = new SqlDataAdapter(command);
+                    DataSet ds = new DataSet();
+                    adapter.Fill(ds, "Table");
+                    if (ds.Tables["Table"].Rows.Count > 0)
                     {
                         bufferBlock.Post(ds.Tables["Table"].Copy());
                     }
-                } while (numberOfRowsRead == batchSize);
+                }
             }
-        }
 
-        bufferBlock.Complete();
-        actionBlock.Completion.Wait();
+            bufferBlock.Complete();
+            actionBlock.Completion.Wait();
+        }
     }
 
     static void BulkInsertData(DataTable data, string destinationConnectionString)
