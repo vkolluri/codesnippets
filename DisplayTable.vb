1e26d1c8dac95764f0fb883dd6afc0880c5689fb
vkolluri <31540929+vkolluri@users.noreply.github.com>
2023-12-19 10:20:25 -0500

    Update DisplayTable.vb

--- DisplayTable.vb
+++ DisplayTable.vb
@@ -1,3 +1,14 @@
-bcp "select * from tempdb..vw_bcpMasterSysobjects
-          order by crdate desc, crtime desc"
-       queryout c:\bcp\sysobjects.txt -c -t, -T -S
+CREATE PROCEDURE ExportDataToCSV
+    @procedureName NVARCHAR(128),
+    @outputFilePath NVARCHAR(256)
+AS
+BEGIN
+    DECLARE @powershellCommand NVARCHAR(MAX)
+    DECLARE @sqlCommand NVARCHAR(MAX)
+
+    SET @sqlCommand = 'sqlcmd -S your_server_name -d your_database_name -U your_username -P your_password -Q "EXEC ' + @procedureName + '" -s"," -W -h -1 | findstr /R /V "^_"'
+
+    SET @powershellCommand = 'powershell -Command "' + @sqlCommand + ' | Out-File -FilePath ' + @outputFilePath + ' -Encoding utf8BOM"'
+
+    EXEC xp_cmdshell @powershellCommand
+END
