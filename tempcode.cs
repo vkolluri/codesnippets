05fa7d82c31ba73d96de4856a461d233a0ca54d6
vkolluri <31540929+vkolluri@users.noreply.github.com>
2024-01-24 15:47:41 -0500

    Update tempcode.cs

--- tempcode.cs
+++ tempcode.cs
@@ -1,37 +1,13 @@
-<?xml version="1.0" encoding="utf-8"?>
-<configuration>
-	<configSections>
-		<section name="quartz" type="System.Configuration.NameValueSectionHandler, System, Version=2.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089" />
-	</configSections>
-	<startup>
-		<supportedRuntime version="v4.0" sku=".NETFramework,Version=v4.7.2" />
-	</startup>
-	<quartz>
-		<add key="quartz.scheduler.instanceName" value="MyScheduler" />
-		<add key="quartz.threadPool.type" value="Quartz.Simpl.SimpleThreadPool, Quartz" />
-		<add key="quartz.threadPool.threadCount" value="10" />
-		<add key="quartz.jobStore.misfireThreshold" value="60000" />
-		
-	</quartz>
-	
-  <runtime>
-    <assemblyBinding xmlns="urn:schemas-microsoft-com:asm.v1">
-      <dependentAssembly>
-        <assemblyIdentity name="Newtonsoft.Json" publicKeyToken="30ad4fe6b2a6aeed" culture="neutral" />
-        <bindingRedirect oldVersion="0.0.0.0-13.0.0.0" newVersion="13.0.0.0" />
-      </dependentAssembly>
-      <dependentAssembly>
-        <assemblyIdentity name="Quartz" publicKeyToken="f6b8c98a402cc8a4" culture="neutral" />
-        <bindingRedirect oldVersion="0.0.0.0-3.8.0.0" newVersion="3.8.0.0" />
-      </dependentAssembly>
-      <dependentAssembly>
-        <assemblyIdentity name="System.Memory" publicKeyToken="cc7b13ffcd2ddd51" culture="neutral" />
-        <bindingRedirect oldVersion="0.0.0.0-4.0.1.1" newVersion="4.0.1.1" />
-      </dependentAssembly>
-      <dependentAssembly>
-        <assemblyIdentity name="Microsoft.Owin" publicKeyToken="31bf3856ad364e35" culture="neutral" />
-        <bindingRedirect oldVersion="0.0.0.0-2.1.0.0" newVersion="2.1.0.0" />
-      </dependentAssembly>
-    </assemblyBinding>
-  </runtime>
-</configuration>
+<quartz>
+        <add key="quartz.scheduler.instanceName" value="MyScheduler" />
+        <add key="quartz.threadPool.type" value="Quartz.Simpl.SimpleThreadPool, Quartz" />
+        <add key="quartz.threadPool.threadCount" value="10" />
+        <add key="quartz.jobStore.misfireThreshold" value="60000" />
+        <add key="quartz.jobStore.type" value="Quartz.Impl.AdoJobStore.JobStoreTX, Quartz" />
+        <add key="quartz.jobStore.useProperties" value="false" />
+        <add key="quartz.jobStore.dataSource" value="default" />
+        <add key="quartz.jobStore.tablePrefix" value="QRTZ_" />
+        <add key="quartz.dataSource.default.connectionString" value="[Your database connection string here]" />
+        <add key="quartz.dataSource.default.provider" value="SqlServer-41" /> <!-- Change depending on your database type -->
+        <!-- Other settings as needed -->
+    </quartz>
