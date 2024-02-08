e10194b43c07d4592f8e81051c81c42f0037e630
vkolluri <31540929+vkolluri@users.noreply.github.com>
2024-02-08 17:36:32 -0500

    Update tempcode.cs

--- tempcode.cs
+++ tempcode.cs
@@ -1,62 +1,43 @@
-using Quartz;
-using System;
-using System.Data.SqlClient;
-using System.Threading.Tasks;
+<?xml version="1.0" encoding="utf-8" ?>
+<log4net>
+    <!-- Appender for DEBUG messages -->
+    <appender name="DebugAppender" type="log4net.Appender.RollingFileAppender">
+        <file value="logs/debug.log" />
+        <appendToFile value="true" />
+        <rollingStyle value="Size" />
+        <maxSizeRollBackups value="5" />
+        <maximumFileSize value="10MB" />
+        <staticLogFileName value="true" />
+        <layout type="log4net.Layout.PatternLayout">
+            <conversionPattern value="%date [%thread] %-5level %logger - %message%newline" />
+        </layout>
+        <filter type="log4net.Filter.LevelRangeFilter">
+            <levelMin value="DEBUG" />
+            <levelMax value="DEBUG" />
+        </filter>
+    </appender>
 
-namespace QuartzConsoleApp
-{
-    public class StoredProcedureJob : IJob
-    {
-        public async Task Execute(IJobExecutionContext context)
-        {
-            // Retrieve job details, like the connection string and stored procedure name
-            var connectionString = context.JobDetail.JobDataMap.GetString("connectionString");
-            var storedProcedureName = context.JobDetail.JobDataMap.GetString("storedProcedureName");
+    <!-- Appender for INFO, WARN, ERROR, and FATAL messages -->
+    <appender name="InfoAppender" type="log4net.Appender.RollingFileAppender">
+        <file value="logs/info.log" />
+        <appendToFile value="true" />
+        <rollingStyle value="Size" />
+        <maxSizeRollBackups value="5" />
+        <maximumFileSize value="10MB" />
+        <staticLogFileName value="true" />
+        <layout type="log4net.Layout.PatternLayout">
+            <conversionPattern value="%date [%thread] %-5level %logger - %message%newline" />
+        </layout>
+        <filter type="log4net.Filter.LevelRangeFilter">
+            <levelMin value="INFO" />
+            <levelMax value="FATAL" />
+        </filter>
+    </appender>
 
-            try
-            {
-                using (var connection = new SqlConnection(connectionString))
-                {
-                    await connection.OpenAsync();
-                    using (var command = new SqlCommand(storedProcedureName, connection))
-                    {
-                        command.CommandType = System.Data.CommandType.StoredProcedure;
-                        // Add any parameters to the command here
-                        await command.ExecuteNonQueryAsync();
-                    }
-                }
-            }
-            catch (SqlException ex)
-            {
-                // Handle SQL-specific errors
-                // Log the exception and/or take specific actions
-                throw new JobExecutionException($"SQL error occurred executing stored procedure: {ex.Message}", ex, false);
-            }
-            catch (Exception ex)
-            {
-                // Handle non-SQL errors
-                // Log the exception and/or take specific actions
-                throw new JobExecutionException($"Error occurred executing stored procedure: {ex.Message}", ex, false);
-            }
-        }
-    }
-}
-
- private static async Task PrintAllJobs(IScheduler scheduler)
-    {
-        var jobGroups = await scheduler.GetJobGroupNames();
-        foreach (var group in jobGroups)
-        {
-            var groupMatcher = Quartz.Impl.Matchers.GroupMatcher<JobKey>.GroupContains(group);
-            var jobKeys = await scheduler.GetJobKeys(groupMatcher);
-            foreach (var jobKey in jobKeys)
-            {
-                var detail = await scheduler.GetJobDetail(jobKey);
-                Console.WriteLine("Job details: ");
-                Console.WriteLine($"Job Name: {detail.Key.Name}");
-                Console.WriteLine($"Job Group: {detail.Key.Group}");
-                Console.WriteLine($"Job Type: {detail.JobType}");
-                // You can also print out other details about the job here
-            }
-        }
-    }
+    <!-- Root logger setup -->
+    <root>
+        <level value="DEBUG" />
+        <appender-ref ref="DebugAppender" />
+        <appender-ref ref="InfoAppender" />
+    </root>
+</log4net>
