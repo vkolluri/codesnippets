e9aa42d2348bbd0c4c85821a49d8fbf916f672ec
vkolluri <31540929+vkolluri@users.noreply.github.com>
2024-04-15 12:36:59 -0400

    Update tempcode.cs

--- tempcode.cs
+++ tempcode.cs
@@ -1,35 +1,5 @@
-using System;
-using System.Collections.Generic;
-
-class Program
-{
-    static void Main()
-    {
-        // The original string
-        var originalString = "Hello, this is a test. This function will replace certain words.";
-
-        // Dictionary with old values as keys and new values as replacements
-        var replacements = new Dictionary<string, string>
-        {
-            {"Hello", "Hi"},
-            {"test", "demo"},
-            {"function", "method"},
-            {"certain", "specific"}
-        };
-
-        // Replace the values based on the dictionary
-        var resultString = ReplaceValues(originalString, replacements);
-
-        // Output the result
-        Console.WriteLine(resultString);
-    }
-
-    static string ReplaceValues(string input, Dictionary<string, string> replacements)
-    {
-        foreach (var pair in replacements)
-        {
-            input = input.Replace(pair.Key, pair.Value);
-        }
-        return input;
-    }
-}
+SELECT
+    ('<td>' + ISNULL(CAST(col1 AS NVARCHAR(MAX)), '') + '</td>' +
+     '<td>' + ISNULL(CAST(col2 AS NVARCHAR(MAX)), '') + '</td>') AS 'tr'
+FROM table1
+FOR XML PATH(''), ROOT('table')
